c0bc4f509d9c10c56a6b947eac8f744bf9f1d354
Zhaolong Zhu <zhaolong@fb.com>
2022-04-24 14:44:54 -0400

    3.11

---
+++ README.md
@@ -0,0 +1,2 @@
+## Introduction
+[Zero To Production In Rust](https://www.zero2prod.com/) Practice.
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::net::TcpListener;
+use sqlx::PgPool;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
